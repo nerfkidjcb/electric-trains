414e81f76faab77fdf7bd65aca6affa019db2df5
Joe <nerfkidjcb@gmail.com>
2024-10-31 19:04:40 +0000

    Implement research behind fulgora and electromagnetism

--- data-updates.lua
+++ data-updates.lua
@@ -516,7 +516,7 @@ data:extend({
 
 
   -- If the Space Exploration mod is installed, make compatibility changes.
-  if mods["space-exploration"]  or  mods["space-age"] then
+  if mods["space-exploration"]  or  (mods["space-age"] and settings.startup["lock-behind-fulgora"].value == "false") then
     -- Update the tech-electric-trains technology.
     -- Empty the ingredients table.
     data.raw["technology"]["tech-electric-trains"].unit.ingredients = {}
@@ -526,6 +526,35 @@ data:extend({
     data.raw["technology"]["tech-electric-trains"].prerequisites = {}
     -- Add the new science prerequisites.
     if mods["space-age"] then
+      local one_gravity_condition =
+      {
+        {
+          property = "gravity",
+          min = 1
+        }
+      }
+      data.raw["locomotive"]["electric-locomotive"].surface_conditions = one_gravity_condition
+      data.raw["locomotive"]["electric-locomotive-wagon"].surface_conditions = one_gravity_condition
+      data.raw["cargo-wagon"]["electric-cargo-wagon"].surface_conditions = one_gravity_condition
+      data.raw["fluid-wagon"]["electric-fluid-wagon"].surface_conditions = one_gravity_condition
+      --------------------------- Add weights and default import planets --------------------------- --
+      data.raw["item-with-entity-data"]["electric-locomotive"].default_import_location = "fulgora"
+      data.raw["item-with-entity-data"]["electric-locomotive"].weight = 1000*kg
+      data.raw["item-with-entity-data"]["electric-locomotive-wagon"].default_import_location = "fulgora"
+      data.raw["item-with-entity-data"]["electric-locomotive-wagon"].weight = 1000*kg
+      data.raw["item-with-entity-data"]["electric-cargo-wagon"].default_import_location = "fulgora"
+      data.raw["item-with-entity-data"]["electric-cargo-wagon"].weight = 1000*kg
+      data.raw["item-with-entity-data"]["electric-fluid-wagon"].default_import_location = "fulgora"
+      data.raw["item-with-entity-data"]["electric-fluid-wagon"].weight = 1000*kg
+      data.raw["item"]["electric-train-battery-charging-station"].default_import_location = "fulgora"
+      data.raw["item"]["electric-train-battery-charging-station"].weight = 100*kg
+      data.raw["item"]["electric-train-battery-pack"].default_import_location = "fulgora"
+      data.raw["item"]["electric-train-battery-pack"].weight = 10*kg
+      data.raw["item"]["electric-train-discharged-battery-pack"].weight = 10*kg
+      if settings.startup["train-battery-decay-enable-setting"].value then
+        data.raw["item"]["electric-train-destroyed-battery-pack"].weight = 10*kg
+      end
+      
       data.raw["technology"]["tech-electric-trains"].prerequisites = {"logistic-science-pack", "chemical-science-pack", "electric-engine"}
     else
       data.raw["technology"]["tech-electric-trains"].prerequisites = {"logistic-science-pack", "chemical-science-pack", "electric-engine"}
@@ -601,22 +630,22 @@ data:extend({
     data.raw["recipe"]["efficiency-battery-pack"].ingredients = {{type = "item", name = "efficiency-module", amount = 1}, {type = "item", name = "battery", amount = 5}}
 
     -- Replace all processing units with advanced circuits, since blue chips are much  harder to get in SE.
-data:extend({
-    {
-        type = "recipe",
-        name = "recipe-electric-locomotive",
-        ingredients = {
-            {type = "item", name = "steel-plate", amount = 40},
-            {type = "item", name = "advanced-circuit", amount = 10},
-            {type = "item", name = "electric-engine-unit", amount = 50},
-            {type = "item", name = "locomotive", amount = 1},
-        },
-        results = {
-          {type="item", name="electric-locomotive", amount=1}
-        },
-        enabled = false
-    }
-})
+    data:extend({
+        {
+            type = "recipe",
+            name = "recipe-electric-locomotive",
+            ingredients = {
+                {type = "item", name = "steel-plate", amount = 40},
+                {type = "item", name = "advanced-circuit", amount = 10},
+                {type = "item", name = "electric-engine-unit", amount = 50},
+                {type = "item", name = "locomotive", amount = 1},
+            },
+            results = {
+              {type="item", name="electric-locomotive", amount=1}
+            },
+            enabled = false
+        }
+    })
     -- Electric Locomotive Wagon
     data.raw["recipe"]["recipe-electric-locomotive-wagon"].ingredients = {
       {type = "item", name = "steel-plate", amount = 40},
@@ -688,4 +717,152 @@ data:extend({
       data.raw["recipe"]["acceleration-battery-pack-refurbish"].category = "hard-recycling"
       data.raw["recipe"]["efficiency-battery-pack-refurbish"].category = "hard-recycling"
     end
-  end
+
+  elseif mods["space-age"] and settings.startup["lock-behind-fulgora"].value == "true" then
+    local one_gravity_condition =
+    {
+      {
+        property = "gravity",
+        min = 1
+      }
+    }
+    data.raw["locomotive"]["electric-locomotive"].surface_conditions = one_gravity_condition
+    data.raw["locomotive"]["electric-locomotive-wagon"].surface_conditions = one_gravity_condition
+    data.raw["cargo-wagon"]["electric-cargo-wagon"].surface_conditions = one_gravity_condition
+    data.raw["fluid-wagon"]["electric-fluid-wagon"].surface_conditions = one_gravity_condition
+    -- --------------------------- Add weights and default import planets --------------------------- --
+    data.raw["item-with-entity-data"]["electric-locomotive"].default_import_location = "fulgora"
+    data.raw["item-with-entity-data"]["electric-locomotive"].weight = 1000*kg
+    data.raw["item-with-entity-data"]["electric-locomotive-wagon"].default_import_location = "fulgora"
+    data.raw["item-with-entity-data"]["electric-locomotive-wagon"].weight = 1000*kg
+    data.raw["item-with-entity-data"]["electric-cargo-wagon"].default_import_location = "fulgora"
+    data.raw["item-with-entity-data"]["electric-cargo-wagon"].weight = 1000*kg
+    data.raw["item-with-entity-data"]["electric-fluid-wagon"].default_import_location = "fulgora"
+    data.raw["item-with-entity-data"]["electric-fluid-wagon"].weight = 1000*kg
+    data.raw["item"]["electric-train-battery-charging-station"].default_import_location = "fulgora"
+    data.raw["item"]["electric-train-battery-charging-station"].weight = 100*kg
+    data.raw["item"]["electric-train-battery-pack"].default_import_location = "fulgora"
+    data.raw["item"]["electric-train-battery-pack"].weight = 10*kg
+    data.raw["item"]["electric-train-discharged-battery-pack"].weight = 10*kg
+    if settings.startup["train-battery-decay-enable-setting"].value then
+      data.raw["item"]["electric-train-destroyed-battery-pack"].weight = 10*kg
+    end
+    -- ----------------- Change the recipes now to use the electromagentics facility ---------------- --
+    data.raw["recipe"]["recipe-electric-locomotive"].subgroup = "fulgora-processes"
+    data.raw["recipe"]["recipe-electric-locomotive"].category = "electromagnetics"
+    data.raw["recipe"]["recipe-electric-locomotive"].allow_productivity = true
+    data.raw["recipe"]["recipe-electric-locomotive"].ingredients = {{type="item", name="steel-plate", amount=40},{type="item", name="electric-engine-unit", amount=50},{type="item", name="advanced-circuit", amount=10}, {type="item", name="locomotive", amount=1}, {type="item", name="superconductor", amount=10}, {type="item", name="supercapacitor", amount=5}}
+    data.raw["recipe"]["recipe-electric-locomotive-wagon"].subgroup = "fulgora-processes"
+    data.raw["recipe"]["recipe-electric-locomotive-wagon"].category = "electromagnetics"
+    data.raw["recipe"]["recipe-electric-locomotive-wagon"].allow_productivity = true
+    data.raw["recipe"]["recipe-electric-locomotive-wagon"].ingredients = {{type="item", name="steel-plate", amount=40},{type="item", name="electric-engine-unit", amount=50},{type="item", name="advanced-circuit", amount=10}, {type="item", name="locomotive", amount=1}, {type="item", name="superconductor", amount=10}, {type="item", name="supercapacitor", amount=5}}
+    data.raw["recipe"]["recipe-electric-cargo-wagon"].subgroup = "fulgora-processes"
+    data.raw["recipe"]["recipe-electric-cargo-wagon"].category = "electromagnetics"
+    data.raw["recipe"]["recipe-electric-cargo-wagon"].allow_productivity = true
+    data.raw["recipe"]["recipe-electric-cargo-wagon"].ingredients = {{type="item", name="steel-plate", amount=20},{type="item", name="electric-engine-unit", amount=5},{type="item", name="advanced-circuit", amount=10}, {type="item", name="cargo-wagon", amount=1}, {type="item", name="superconductor", amount=2}, {type="item", name="supercapacitor", amount=1}}
+    data.raw["recipe"]["recipe-electric-fluid-wagon"].subgroup = "fulgora-processes"
+    data.raw["recipe"]["recipe-electric-fluid-wagon"].category = "electromagnetics"
+    data.raw["recipe"]["recipe-electric-fluid-wagon"].ingredients = {{type="item", name="steel-plate", amount=20},{type="item", name="electric-engine-unit", amount=5},{type="item", name="advanced-circuit", amount=10}, {type="item", name="fluid-wagon", amount=1}, {type="item", name="superconductor", amount=2}, {type="item", name="supercapacitor", amount=1}}
+    data.raw["recipe"]["electric-train-battery-charging-station"].allow_productivity = true
+    data.raw["recipe"]["electric-train-battery-charging-station"].subgroup = "fulgora-processes"
+    data.raw["recipe"]["electric-train-battery-charging-station"].category = "electromagnetics"
+    data.raw["recipe"]["electric-train-battery-charging-station"].ingredients = {{type="item", name="processing-unit", amount=10},{type="item", name="steel-plate", amount=10},{type="item", name="superconductor", amount=5},{type="item", name="holmium-plate", amount=2}}
+    -- epxerimental charging station
+    data.raw["recipe"]["experimental-electric-train-battery-charging-station"].allow_productivity = true
+    data.raw["recipe"]["experimental-electric-train-battery-charging-station"].subgroup = "fulgora-processes"
+    data.raw["recipe"]["experimental-electric-train-battery-charging-station"].category = "electromagnetics"
+    data.raw["recipe"]["experimental-electric-train-battery-charging-station"].ingredients = {{type="item", name="processing-unit", amount=50},{type="item", name="steel-plate", amount=10},{type="item", name="superconductor", amount=10},{type="item", name="holmium-plate", amount=5}}
+    -- normal powerpacks
+    data.raw["recipe"]["electric-train-battery-pack"].allow_productivity = true
+    data.raw["recipe"]["electric-train-battery-pack"].subgroup = "fulgora-processes"
+    data.raw["recipe"]["electric-train-battery-pack"].category = "electromagnetics"
+    data.raw["recipe"]["electric-train-battery-pack"].ingredients = {{type="item", name="supercapacitor", amount=1},{type="item", name="superconductor", amount=2},{type="item", name="holmium-plate", amount=2}}
+    -- alkaline powerpacks
+    data.raw["recipe"]["recipe-electric-train-alkaline-battery-pack"].allow_productivity = true
+    data.raw["recipe"]["recipe-electric-train-alkaline-battery-pack"].subgroup = "fulgora-processes"
+    data.raw["recipe"]["recipe-electric-train-alkaline-battery-pack"].category = "electromagnetics"
+    data.raw["recipe"]["recipe-electric-train-alkaline-battery-pack"].ingredients = {{type="item", name="supercapacitor", amount=1},{type="item", name="superconductor", amount=1},{type="item", name="holmium-plate", amount=2}}
+    -- speed powerpacks
+    data.raw["recipe"]["speed-battery-pack"].allow_productivity = true
+    data.raw["recipe"]["speed-battery-pack"].subgroup = "fulgora-processes"
+    data.raw["recipe"]["speed-battery-pack"].category = "electromagnetics"
+    data.raw["recipe"]["speed-battery-pack"].ingredients = {{type="item", name="supercapacitor", amount=2},{type="item", name="superconductor", amount=3},{type="item", name="holmium-plate", amount=5}, {type="item", name="speed-module", amount=1}}
+    -- acceleration powerpacks
+    data.raw["recipe"]["acceleration-battery-pack"].allow_productivity = true
+    data.raw["recipe"]["acceleration-battery-pack"].subgroup = "fulgora-processes"
+    data.raw["recipe"]["acceleration-battery-pack"].category = "electromagnetics"
+    data.raw["recipe"]["acceleration-battery-pack"].ingredients = {{type="item", name="supercapacitor", amount=1},{type="item", name="superconductor", amount=4},{type="item", name="holmium-plate", amount=5}, {type="item", name="productivity-module", amount=1}}
+    -- efficiency powerpacks
+    data.raw["recipe"]["efficiency-battery-pack"].allow_productivity = true
+    data.raw["recipe"]["efficiency-battery-pack"].subgroup = "fulgora-processes"
+    data.raw["recipe"]["efficiency-battery-pack"].category = "electromagnetics"
+    data.raw["recipe"]["efficiency-battery-pack"].ingredients = {{type="item", name="supercapacitor", amount=3},{type="item", name="superconductor", amount=2},{type="item", name="holmium-plate", amount=5}, {type="item", name="efficiency-module", amount=1}}
+    
+    -- ------------------------------ Update tech tree ------------------------------ --
+    data.raw["technology"]["tech-electric-trains"].unit.ingredients = {}
+    data.raw["technology"]["tech-electric-trains"].unit.ingredients = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}, {"production-science-pack", 1}, {"utility-science-pack", 1}, {"space-science-pack", 1}, {"electromagnetic-science-pack", 1}}
+    data.raw["technology"]["tech-electric-trains"].unit.count = 2000
+    data.raw["technology"]["tech-electric-trains"].prerequisites = {"processing-unit", "electromagnetic-plant", "railway", "holmium-processing", "electromagnetic-science-pack"}
+    -- Update alkaline battery pack tech.
+    -- Empty the ingredients table.
+    data.raw["technology"]["tech-alkaline-battery-pack"].unit.ingredients = {}
+    data.raw["technology"]["tech-alkaline-battery-pack"].unit.ingredients = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}, {"production-science-pack", 1}, {"utility-science-pack", 1}, {"space-science-pack", 1}, {"electromagnetic-science-pack", 1}}
+    data.raw["technology"]["tech-alkaline-battery-pack"].unit.count = 2000
+    -- Remove the old science prerequisites.
+    data.raw["technology"]["tech-alkaline-battery-pack"].prerequisites = {}
+    -- Add the new science prerequisites.
+    data.raw["technology"]["tech-alkaline-battery-pack"].prerequisites = {"tech-electric-trains"}
+    
+    -- Update the tech-electric-trains-experimental-charging technology.
+    -- Empty the ingredients table.
+    data.raw["technology"]["tech-electric-trains-experimental-charging"].unit.ingredients = {}
+    data.raw["technology"]["tech-electric-trains-experimental-charging"].unit.ingredients = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}, {"production-science-pack", 1}, {"utility-science-pack", 1}, {"space-science-pack", 1}, {"electromagnetic-science-pack", 1}}
+    data.raw["technology"]["tech-electric-trains-experimental-charging"].unit.count = 2000
+    -- Remove the old science prerequisites.
+    data.raw["technology"]["tech-electric-trains-experimental-charging"].prerequisites = {}
+    -- Add the new science prerequisites.
+    data.raw["technology"]["tech-electric-trains-experimental-charging"].prerequisites = {"tech-alkaline-battery-pack", "tech-electric-trains", "tech-electric-locomotive-wagon"}
+
+    -- Update the tech-electric-locomotive-wagon technology.
+    -- Empty the ingredients table.
+    data.raw["technology"]["tech-electric-locomotive-wagon"].unit.ingredients = {}
+    data.raw["technology"]["tech-electric-locomotive-wagon"].unit.ingredients = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}, {"production-science-pack", 1}, {"utility-science-pack", 1}, {"space-science-pack", 1}, {"electromagnetic-science-pack", 1}}
+    data.raw["technology"]["tech-electric-locomotive-wagon"].unit.count = 2000
+    -- Remove the old science prerequisites.
+    data.raw["technology"]["tech-electric-locomotive-wagon"].prerequisites = {}
+    -- Add the new science prerequisites.
+    data.raw["technology"]["tech-electric-locomotive-wagon"].prerequisites = {"tech-electric-trains"}  
+
+    -- Update the tech-speed-battery-pack technology.
+    -- Empty the ingredients table.
+    data.raw["technology"]["tech-speed-battery-pack"].unit.ingredients = {}
+    data.raw["technology"]["tech-speed-battery-pack"].unit.ingredients = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}, {"production-science-pack", 1}, {"utility-science-pack", 1}, {"space-science-pack", 1}, {"electromagnetic-science-pack", 1}}
+    data.raw["technology"]["tech-speed-battery-pack"].unit.count = 4000
+    -- Remove the old science prerequisites.
+    data.raw["technology"]["tech-speed-battery-pack"].prerequisites = {}
+    -- Add the new science prerequisites.
+    data.raw["technology"]["tech-speed-battery-pack"].prerequisites = {"tech-electric-trains", "tech-electric-trains-experimental-charging"}
+
+    -- Update the tech-acceleration-battery-pack technology.
+    -- Empty the ingredients table.
+    data.raw["technology"]["tech-acceleration-battery-pack"].unit.ingredients = {}
+    data.raw["technology"]["tech-acceleration-battery-pack"].unit.ingredients = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}, {"production-science-pack", 1}, {"utility-science-pack", 1}, {"space-science-pack", 1}, {"electromagnetic-science-pack", 1}}
+    data.raw["technology"]["tech-acceleration-battery-pack"].unit.count = 4000
+    -- Remove the old science prerequisites.
+    data.raw["technology"]["tech-acceleration-battery-pack"].prerequisites = {}
+    -- Add the new science prerequisites.
+    data.raw["technology"]["tech-acceleration-battery-pack"].prerequisites = {"tech-electric-trains", "tech-electric-trains-experimental-charging"}
+
+    -- Update the tech-efficiency-battery-pack technology.
+    -- Empty the ingredients table.
+    data.raw["technology"]["tech-efficiency-battery-pack"].unit.ingredients = {}
+    data.raw["technology"]["tech-efficiency-battery-pack"].unit.ingredients = {{"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"chemical-science-pack", 1}, {"production-science-pack", 1}, {"utility-science-pack", 1}, {"space-science-pack", 1}, {"electromagnetic-science-pack", 1}}
+    data.raw["technology"]["tech-efficiency-battery-pack"].unit.count = 4000
+    -- Remove the old science prerequisites.
+    data.raw["technology"]["tech-efficiency-battery-pack"].prerequisites = {}
+    -- Add the new science prerequisites.
+    data.raw["technology"]["tech-efficiency-battery-pack"].prerequisites = {"tech-electric-trains", "tech-electric-trains-experimental-charging"}
+
+    -- Just remove the braking force infinite techs.
+    data.raw["technology"]["tech-electric-trains-braking-force-1"] = nil
+  end  
